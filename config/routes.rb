ea554315ee40ca441f35b7c76fd52478c0131970
Ebowe Blessing <blessing.ebowe@andela.com>
2016-01-13 14:43:29 +0100

    Completion of testing
    WHY:
    To make sure framework is 360 degree tested.
    
    This issue if fixed by:
    Writing Features test for the gem using this app as a test model.
    Writing Controller tests that are done with this app.
    Also writing requests and routing tests, making use of routes in this app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 PostApplication.routes.draw do
-  # get "/posts", to: "posts#index"
-  # get "/posts/new", to: "posts#new"
-  # get "/posts/:id", to: "posts#show"
-  # get("/posts/edit/:id", to: "posts#edit")
-  # post("/posts", to: "posts#create")
-  # patch("/posts/:id", to: "posts#update")
-  # put("/posts/:id", to: "posts#update")
-  # delete("/posts/:id", to: "posts#destroy")
   get "/no_template", to: "posts#no_template"
   resources(:posts)
 end
